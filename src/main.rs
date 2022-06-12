41f2ece229dd3cfc6cf8b205fec75f794be80076
James Perry <jaupe@protonmail.com>
2022-06-12 01:17:13 +0100

    change binding

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(db_state.clone())
             .service(eval)
     })
-    .bind(("127.0.0.1", 8686))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
